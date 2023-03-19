c0bdc4cf6c4865575823dc80ab31f45f963a0b20
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-03-19 14:51:26 +0530

    truncation

--- typeCasting.java
+++ typeCasting.java
@@ -1,8 +1,12 @@
 public class typeCasting {
     public static void main(String[] args) {
-        int a=45;
-        double b ;
-        b=a;
-        System.out.println(b);
+        // int a=45;
+        // double b ;
+        // b=a;
+        // System.out.println(b);
+        int a = 65;
+        int b= 5;
+        float res = a+b;
+        System.out.println(res); //truncate automatic without suffix
     }
 }
