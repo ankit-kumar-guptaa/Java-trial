000ac133f0cb4bc6de2a1d968703c320d7e90c86
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-03-19 22:51:16 +0530

    6

--- typeCasting.java
+++ typeCasting.java
@@ -4,9 +4,20 @@ public class typeCasting {
         // double b ;
         // b=a;
         // System.out.println(b);
-        int a = 65;
-        int b= 5;
-        float res = a+b;
-        System.out.println(res); //truncate automatic without suffix
+        // int a = 65;
+        // int b= 5;
+        // float res = a+b;
+        // System.out.println(res); //truncate automatic without suffix
+        
+        // double num1=45.5;
+        // int num2 ;
+        // num2 = (int)num1;// explicit typecasting
+        // System.out.println(num2);
+
+        int a=12;
+        int b=5;
+        float res=a/b;
+        System.out.println(res);
+
     }
 }
