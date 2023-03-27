126ad97420349e42ccf79ccadb025b1e7873884b
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-03-27 23:23:14 +0530

    update ternary

--- Ternary_op.java
+++ Ternary_op.java
@@ -1,9 +1,31 @@
 public class Ternary_op {
     public static void main(String[] args) {
         int a=2;
+        int b=1;
+        int c=3;
         String res=(a%2==0)? "even number":"odd number";
         System.out.println(res);
-        
+        int ank = (a<b)?(a<c? a:b) : (b<c? b: c);
+        System.out.println(ank);
+        if(a<b)
+        {
+            if(a<b){
+                System.out.println(a);
+            }
+            else{
+                System.out.println(c);
+            }
+        }
+        else{
+            if(b<c)
+            {
+                System.out.println(b);
+            }
+            else{
+                System.out.println(c);
+            }
+        }
+
     }
     
 }
