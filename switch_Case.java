b5dbbf2b1c064ffbc17bf9195c53f5b86cda176a
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-03-27 23:44:48 +0530

    switch case code

---
+++ switch_Case.java
@@ -0,0 +1,18 @@
+public class switch_Case {
+    public static void main(String[] args) {
+        int num = 400;
+        switch (num) {
+            case 20:
+                System.out.println("case1");
+                break;
+            case 100:
+                System.out.println("case2");
+                break;
+            case 300:
+                System.out.println("case3");
+                break;
+                default: System.out.println("no match any case");
+        }
+    }
+
+}
