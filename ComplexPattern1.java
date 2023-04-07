28874d48dbec54a1dcaf5eb540517f0700c29bd1
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-04-07 18:42:16 +0530

    add java assignment

--- ComplexPattern1.java
+++ ComplexPattern1.java
@@ -1,11 +1,11 @@
 public class ComplexPattern1 {
     public static void main(String[] args) {
-        int n = 11;
+        int n = 21;
         for(int i=0; i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(i==j||i+j==n-1|| i+j==(n-1)/2||i==0 || j==0 ||i==n-1||j==n-1)
+                if(i==j||i+j==n-1|| i+j==(n-1)/2||i-j==(n-1)/2||i==0 || j==0 || i==n-1||j==n-1 || )
                 {
                     System.out.print("*");
                 }
