6eed41885c58fca940c1fbe0004382b201f9e37c
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-04-15 10:45:21 +0530

    improve code

---
+++ OOPS in java/Method_overloading.java
@@ -0,0 +1,16 @@
+class Calc{
+    public int add(int n1, int n2)
+    {
+        int result = n1+n2;
+        return result;
+    }
+}
+public class Method_overloading {
+        public static void main(String[] args) {
+            Calc obj= new Calc();
+            int res=obj.add(65, 5);
+            System.out.println(res);
+            
+        }
+    
+}
---
+++ OOPS in java/ObjectCreate.java
@@ -0,0 +1,13 @@
+// package OOPS in java;
+
+public class ObjectCreate {
+    int a;
+    String name;
+    public static void main(String[] args) {
+        ObjectCreate ank = new ObjectCreate();
+        ank.a=65;
+        System.out.println(ank.a);
+        System.out.println(ank.name);
+        
+    }
+}
